50b29d0c5404f949416f79bdce92e24838f44eb5
kumathescripter <ojaskumar2@gmail.com>
2013-10-05 23:15:48 +0530

    Added a toggle feature

--- survival.py
+++ survival.py
@@ -1,11 +1,32 @@
 from pyspades.constants import *
+from commnads import add, admin, alias
+
+survival_mode = True #Do you have a problem with this vlad?
+SURVIVAL_ENABLED = "Survival mode has been enabled"
+SURVIVAL_DISABLED = "Survival mode has been disabled"
+
+@alias('ts') #Stands for toggle survival
+@admin
+def survival(connection):
+	protocol = connection.protocol
+	survival_mode = not survival_mode
+	if survival_mode:
+		protocol.send_chat(SURVIVAL_ENABLED)
+	else:
+		protocol.send_chat(SURVIVAL_DISABLED)
+		
+add(survival)
+	
 
 def apply_script(protocol, connection, config):
   
   class survivalConnection(connection):
     
 	def spawn(self):
-		self.weapon = RIFLE_WEAPON
-		return connection.spawn(self)
+		if survival_mode: # editted your code a little, hope you don't have a problem
+			self.weapon = RIFLE_WEAPON
+			return connection.spawn(self)
+		else:
+			return connection.spawn(self)
   
   return protocol, survivalConnection
